ad3ed8b78a1d7438eb312dde89dcc5c91bc752ef
Carracedo Gaston <carracedo.gaston@gmail.com>
2019-11-01 16:26:59 -0300

    Screens basic structure

---
+++ src/components/screens/Login/index.js
@@ -0,0 +1,15 @@
+import './style.css'
+import React, { Component } from 'react'
+import MainLayout from '../../layouts/MainLayout'
+
+class Buy extends Component {
+    render() {
+        return (
+            <MainLayout>
+                <h1>Login</h1>
+            </MainLayout>
+        )
+    }
+}
+
+export default Buy
