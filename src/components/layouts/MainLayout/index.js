e8676016a7be0ab35f89e7f3e49755403fca84d0
Carracedo Gaston <carracedo.gaston@gmail.com>
2019-11-01 18:02:28 -0300

    Basic Menu

--- src/components/layouts/MainLayout/index.js
+++ src/components/layouts/MainLayout/index.js
@@ -3,9 +3,27 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 class MainLayout extends Component {
+  constructor(props) {
+    super(props)
+
+    this.MenuShowHide = this.MenuShowHide.bind(this)
+  }
+  MenuShowHide() {
+    document.getElementById('Menu').style.display === 'flex'?
+      document.getElementById('Menu').style.display = 'none':
+      document.getElementById('Menu').style.display = 'flex'       
+  }
+  MenuHide() {
+    document.getElementById('Menu').style.display = 'none'      
+  }
   render() {
     return (
-      <div id="MainLayout">
+      <div id="MainLayout" >
+        <div id="MenuButton" onClick={this.MenuShowHide}>
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+        </div>
         <div id="Menu">
           <ul>
             <li><Link to="/home">Home</Link></li>
