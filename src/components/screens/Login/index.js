1b7d4176501b17546a3683a7817aba0b04e22ca3
Carracedo Gaston <carracedo.gaston@gmail.com>
2019-11-07 09:30:32 -0300

    basic login structure

--- src/components/screens/Login/index.js
+++ src/components/screens/Login/index.js
@@ -6,8 +6,16 @@ class Login extends Component {
   render() {
     return (
       <MainLayout>
-        <div className="Container Login">
-          <h1>Login</h1>
+        <div className="Container">
+          <div id="login">
+            <input className="inputLogin" placeholder="name"></input>
+            <input className="inputLogin" placeholder="password"></input>
+            <input className="inputLogin" placeholder="email"></input>
+          </div>
+          <div id="buttonsLoginContainer">
+            <button className="buttonLogin">Log In</button>
+            <button className="buttonLogin">Create Account</button>
+          </div>
         </div>
       </MainLayout>
     )
