ee54a47d125189e56dc3a55cbb42a5b875b8fe53
yaninamontella <yani_montella@hotmail.com>
2019-11-07 16:59:10 -0300

    gameType screen

--- src/components/screens/GameTypes/index.js
+++ src/components/screens/GameTypes/index.js
@@ -6,9 +6,18 @@ class GameTypes extends Component {
   render() {
     return (
       <MainLayout>
-        <div className="Container GameTypes">
-          <h1>Game Types</h1>
-          <h1>Game 2222</h1>
+        <div className="Container">
+        <div id="selectStatic">      
+            <div className="selectQuestion">Sports</div>
+            <div className="selectQuestion">History</div>
+            <div className="selectQuestion">Movies</div>
+            <div className="selectQuestion">Languages</div>
+            <div className="selectQuestion">Fashion</div>
+            <div className="selectQuestion">Foods</div>
+          </div>
+          <div id="buttonPanel">
+            <div className="buttonBack">Back</div>
+          </div>
         </div>
       </MainLayout>
     )
