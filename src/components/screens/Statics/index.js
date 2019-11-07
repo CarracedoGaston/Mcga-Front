f59ecdd979fb585dfdc604561e0a1c4cc45c5e75
yaninamontella <yani_montella@hotmail.com>
2019-11-07 17:00:28 -0300

    Statics screen

--- src/components/screens/Statics/index.js
+++ src/components/screens/Statics/index.js
@@ -6,8 +6,17 @@ class Statics extends Component {
   render() {
     return (
       <MainLayout>
-        <div className="Container Statics">
-          <h1>Statics</h1>
+        <div className="Container">
+          <div id="selectStatic">      
+            <div className="selectQuestion">question 1</div>
+            <div className="selectQuestion">question 2</div>
+            <div className="selectQuestion">question 3</div>
+            <div className="selectQuestion">question 4</div>
+            <div className="selectQuestion">question 5</div>
+            <div className="selectQuestion">question 6</div>
+            <div className="selectQuestion">question 7</div>
+          </div>
+          <div id="staticsImgExample"></div>
         </div>
       </MainLayout>
     )
