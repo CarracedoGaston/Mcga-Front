b1d1ab6c8844c68bd206206658c62708345e4479
Carracedo Gaston <carracedo.gaston@gmail.com>
2019-11-07 17:01:28 -0300

    Updates

---
+++ precache-manifest.04016a9f1d5ba290a7b2c8f95fb24374.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b432568b1c2057697daa1a2d8a20d64",
+    "url": "/Mcga-Front/index.html"
+  },
+  {
+    "revision": "2719e5e51e0753b41094",
+    "url": "/Mcga-Front/static/css/main.da85ecc7.chunk.css"
+  },
+  {
+    "revision": "c9a443daca43a9bf2f41",
+    "url": "/Mcga-Front/static/js/2.e87069a1.chunk.js"
+  },
+  {
+    "revision": "2719e5e51e0753b41094",
+    "url": "/Mcga-Front/static/js/main.be352bc3.chunk.js"
+  },
+  {
+    "revision": "33dfdfd1405bfe569d3f",
+    "url": "/Mcga-Front/static/js/runtime-main.1051c397.js"
+  },
+  {
+    "revision": "18ddfb49ebbb07b4ac02333b2f801f4b",
+    "url": "/Mcga-Front/static/media/MenuBackGround.18ddfb49.jpeg"
+  },
+  {
+    "revision": "b017ca154a2335899348befae4c44292",
+    "url": "/Mcga-Front/static/media/Player.b017ca15.png"
+  },
+  {
+    "revision": "16cfa9ec8bd135cf76f7e37d8b1925cd",
+    "url": "/Mcga-Front/static/media/Stats.16cfa9ec.png"
+  }
+]);
