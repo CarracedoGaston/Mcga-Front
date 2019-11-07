8404b623b4bfd7a73264df2e778e3baf44d6b59c
yaninamontella <yani_montella@hotmail.com>
2019-11-07 16:59:39 -0300

    Question screen

--- src/components/screens/Questions/index.js
+++ src/components/screens/Questions/index.js
@@ -6,8 +6,19 @@ class Questions extends Component {
   render() {
     return (
       <MainLayout>
-        <div className="Container Questions">
-          <h1>Questions</h1>
+        <div className="Container">
+          <div id="newQuestion">
+            <div id="questioToAnswer">
+              <h1>New question?</h1>
+            </div>  
+            <div className="inputAnswer">Answer 1</div>
+            <div className="inputAnswer">Answer 2</div>
+            <div className="inputAnswer">Answer 3</div>
+          </div>
+          <div id="buttonNewQuestion">
+            <div className="buttonQuestion">Game Type</div>
+            <div className="buttonQuestion">Save Question</div>
+          </div>
         </div>
       </MainLayout>
     )
