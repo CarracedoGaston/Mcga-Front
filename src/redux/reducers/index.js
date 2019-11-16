ed38d5bff7f2fee100c5033f832fcfd436012aff
yaninamontella <yani_montella@hotmail.com>
2019-11-16 17:00:19 -0300

    reducer for statics

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import gameType from './gameType'
 import questions from './questions'
+import statics from './statics'
 import profile from './profile'
 
 export default combineReducers({
     gameType,
     questions,
+    statics,
     profile
 })
