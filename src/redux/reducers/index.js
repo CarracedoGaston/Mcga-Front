1021e49f38422508c960692e182d0ebcdb9e56c2
yaninamontella <yani_montella@hotmail.com>
2019-11-16 17:14:12 -0300

    reducer for login

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux'
 import gameType from './gameType'
 import home from './home'
+import login from './login'
 import questions from './questions'
 import statics from './statics'
 import users from './users'
 
 export default combineReducers({
     gameType,
+    login, 
     questions,
     statics,
     home,
--- src/redux/reducers/login.js
+++ src/redux/reducers/login.js
@@ -1,12 +1,12 @@
 const initialState = {
     isLoading: false,
     error: '',
-    list: [ 
-        {name: 'La china',email: 'mejorprofe@gmail.com'},
-        {name: 'juan',email: 'juancho@gmail.com'},
-        {name: 'mariana',email: 'mariana@gmail.com'},        
-    ]
+    data: {
+        name: 'La china',
+        email: 'mejorprofe@gmail.com'
+    }
 }
+
 
 const reducer = (state=initialState, action) => {
     switch(action.type) {
