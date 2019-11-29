78b9e63ca1b4ddfa7610be7d8db5afafc4b2f441
yaninamontella <yani_montella@hotmail.com>
2019-11-29 14:07:24 -0300

    delete statics from reducer and actions

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux'
 import home from './home'
 import questions from './questions'
-import statics from './statics'
 import users from './users'
 
 export default combineReducers({
   questions,
-  statics,
   home,
   users
 })
