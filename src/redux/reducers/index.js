0ce0bdfc1f4849786c5ec5b560f3845f9748d677
yaninamontella <yani_montella@hotmail.com>
2019-12-07 11:00:24 -0300

    basic structure for Statics Redux

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import home from './home'
 import questions from './questions'
 import users from './users'
+import statics from './statics'
 
 export default combineReducers({
   questions,
   home,
-  users
+  users,
+  statics
 })
